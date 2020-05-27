73541ffd74df779b58157551e0de6f9b9d4d8628
simon <simon.mettag@gmail.com>
2020-05-27 18:51:01 -0500

    mongo finally working with post

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,35 @@
 const express = require('express');
 const path = require('path');
 const {MongoClient} = require('mongodb');
-const assert = require('assert');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 
 const app = new express();
 app.use(express.static(path.join(__dirname,'../frontend')));
-app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 const url = 'mongodb+srv://admin:12345@project-c6b6w.gcp.mongodb.net/test?retryWrites=true&w=majority';
-const client = new MongoClient(url, { useUnifiedTopology: true });
-client.connect(function(err, client) {
-	assert.equal(null, err);
-	console.log("Connected successfully to server");
-  
-	client.close();
-});
 
 app.get('/', (req, res) => {
 	res.sendFile(path.resolve(__dirname, '../frontend/login.html'));;
 });
 
-app.post('/sign_in_form', function(req, res) {
-	const {email, password} = req.body;
-	console.log(email);
-	console.log(password);
-	
-	client.connect(function(err, client) {
-		assert.equal(null, err);
-		console.log("Connected successfully to server");
-	  
-		const db = client.db("Nosesmash");
-		var user = db.collection("users").findOne({'email': email, 'password': password});
-		console.log(user);;
+app.post('/validateSign', async (req, res) => {
+	console.log(req.body.email);
+	console.log(req.body.password);
+
+	var client = await MongoClient.connect(url, { 
+		useNewUrlParser: true, 
+		useUnifiedTopology: true,
 	});
+
+	var db = client.db('Nosesmash');
+	var user = await db.collection("users").findOne({'email': req.body.email, 'password': req.body.password});
+	console.log(user);
+
 	client.close();
+
+	res.json({username: user.username});
 });
 
 app.get('/Home', (req, res) => {
